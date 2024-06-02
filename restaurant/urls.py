3540407cf6715652670dc93ec2058ef4af479529
frealchar@gmail.com <frealchar@gmail.com>
2024-06-02 11:17:23 -0400

    added static files

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('book/', views.book, name='book'),
     path('menu/', views.menu, name='menu')
