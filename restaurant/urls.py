10de3b2ec5e95ac42af53723321fa34d39f38b2c
frealchar@gmail.com <frealchar@gmail.com>
2024-06-04 04:36:23 -0400

    added images

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('book/', views.book, name='book'),
-    path('menu/', views.menu, name='menu')
+    path('menu/', views.menu, name='menu'),
+    path('menu_item/<int:pk>/', views.menu_item, name="menu_item"),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from . forms import BookingForm
+from . models import Menu
+
 # Create your views here.
 def home(request):
     return render(request,'index.html')
@@ -12,12 +14,21 @@ def book(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
+            print(form)
             form.save()
     context = {'form': form}
                          
     return render(request,'book.html', context)
 
 def menu(request):
-    return render(request,'menu.html')
+    data = Menu.objects.all()
+    print(data)
+    pizza = {'menu' : data}
+    print(pizza)
+    return render(request,'menu.html', pizza)
 
+def menu_item(request,pk=None):
+    if pk:
+        menu_item = Menu.objects.get(pk=pk)
+        return render(request, 'menu_item.html', {'menu_item': menu_item} )
 
