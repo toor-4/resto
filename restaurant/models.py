81119296ba972e2469c4880fc04920bc1fcb74e9
frealchar@gmail.com <frealchar@gmail.com>
2024-06-07 05:29:48 -0400

    modified home.html

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,8 +4,8 @@ class Reservation(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
-    date = models.DateField(verbose_name='Date (YYYY-MM-DD)')
-    time = models.TimeField(verbose_name='Time (HH:MM)')
+    date = models.DateField(verbose_name='Date (yyyy-mm-dd)')
+    time = models.TimeField(verbose_name='Time (hh:mm)')
 
     def __str__(self):
         return f'{self.name}'
--- restaurant/static/js/read-more.js
+++
@@ -1,9 +0,0 @@
-<script>
-  function expandParagraph() {
-    var paragraph = document.querySelector("article p");
-    paragraph.classList.remove("collapsed");
-    paragraph.style.maxHeight = paragraph.scrollHeight + "px";
-    var button = document.querySelector("article button");
-    button.style.display = "none";
-  }
-</script>
